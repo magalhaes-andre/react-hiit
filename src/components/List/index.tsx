b608daca3fe5456b75df700620fba5d5e1a259a9
magalhaes-andre <arfddem@gmail.com>
2022-11-29 12:45:33 -0300

    feature: creating list component with static data

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
-//Function Component
+//Function Component --> After React 18, Function components are able to maintain state TODO: Research more on it and add to Readme.MD
 function List() {
 
     return (
         <aside>
-
+            <h2> Today's Exercises </h2>
+            <ul>
+                <li>
+                    <h3>
+                        Chest    
+                    </h3>
+                    <span>
+                        00:04:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        Triceps    
+                    </h3>
+                    <span>
+                        00:05:00
+                    </span>
+                </li>
+            </ul>
         </aside>
     )
 }

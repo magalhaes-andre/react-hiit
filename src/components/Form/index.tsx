bdd1579349c679228de45db9819dbf062fb08e0c
magalhaes-andre <arfddem@gmail.com>
2022-11-29 12:55:11 -0300

    feature: start turning list component into a dynamic concept

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from './components/Button';
+import Button from '../Button';
 
 //Class Component
 class Form extends React.Component {
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,27 +2,33 @@ import React from 'react';
 
 //Function Component --> After React 18, Function components are able to maintain state TODO: Research more on it and add to Readme.MD
 function List() {
-
+    const exercises = [{
+        id: 1,
+        name: "Chest",
+        time: "00:04:00"
+    }, {
+        id: 2,
+        name: "Triceps",
+        time: "00:03:00"
+    
+    }, {
+        id: 3,
+        name: "Quadriceps",
+        time: "00:02:00"
+    }]
+    //TODO: Study difference between using {} or () inside arrow function and how this would affect scope or not.
+    //TODO: Study aside tag
+    //Dynamic renders need to have keys as in li
     return (
         <aside>
             <h2> Today's Exercises </h2>
             <ul>
-                <li>
-                    <h3>
-                        Chest    
-                    </h3>
-                    <span>
-                        00:04:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        Triceps    
-                    </h3>
-                    <span>
-                        00:05:00
-                    </span>
-                </li>
+                {exercises.map((exercise, index) => (
+                    <li key={exercise.id}>
+                        <h3> {exercise.name} </h3>
+                        <span> {exercise.time} </span>
+                    </li>
+                ))}
             </ul>
         </aside>
     )

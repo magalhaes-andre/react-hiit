37cc7364d869515130c167e06ee7161a53bafad4
magalhaes-andre <arfddem@gmail.com>
2022-12-10 02:35:43 -0300

    refactor: re-writing Button component as a Function Component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import { text } from 'stream/consumers';
 import style from './Button.module.scss'
 
-//Class Component
-class Button extends React.Component<{
-    text: String, 
+interface Props {
+    text: String,
     type?: "button" | "submit" | "reset" | undefined,
     onClick?: () => void
-}> {
-    render() {
-        //Defaults to button type if none is informed since 'type?' is optional
-        const { type = "button", onClick} = this.props;
-        return (
-            <button type={type} className={style.button} onClick={onClick}>
-                {this.props.text}
-            </button>
-        )
-    }
 }
 
-export default Button;
+export default function Button({ onClick, type, text }: Props) {
+    return (
+        <button type={type} className={style.button} onClick={onClick}>
+            {text}
+        </button>
+    )
+}

b628e031b83410f4e1b926aa2878680c30b47951
magalhaes-andre <arfddem@gmail.com>
2022-12-10 01:11:19 -0300

    refactor: Stopwatch component is working fine again, added a bunch of TODO's to do.

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { timeToSeconds } from "../../common/utils/time";
 import { IExercise } from "../../types/exercise";
 import Button from "../Button";
@@ -11,12 +11,18 @@ interface Props {
 
 
 export default function Stopwatch({selected}: Props) {
+    //Time to seconds needs to be here, if set on through the if statement, it will through "many re-renders" issue.
+    //For changing states such as the stopwatch itself we should use a hook called useEffect. TODO: Research on useEffect.
+    //TODO: Study difference between Number and number.
+    //TODO: Study component lifecycle as in: componentWillMount, componentDidMount, componentWillUpdate, componentWillUnmount and their relationships with hooks as useLayoutEffect or useEffect for example.
     const [time, setTime] = useState<Number>();
     
-    if(selected?.time) {
-        setTime(timeToSeconds(selected.time))
-    }
-    
+    useEffect(() => {
+        if(selected?.time) {
+            setTime(timeToSeconds(selected.time))
+        }
+    }, [selected])
+
     return (
         <div className={style.stopwatch}>
             <p className={style.title}>Choose an exercise and start the clock.</p>
